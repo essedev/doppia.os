2c971298d6e9d727423d2fccdca79f23e44c6858
Simone Salerno <salernosim@gmail.com>
2022-04-17 20:43:11 +0200

    Separated window from desktop

---
+++ src/desktop.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Window from './window.svelte';
+</script>
+
+<Window name='Window' w=650 h=400/>
+
+<style>
+	:global(body) {
+		overflow: hidden;
+		background-color: #048282;
+	}
+</style>
--- src/window.svelte
+++ src/window.svelte
@@ -1,10 +1,13 @@
 <script>
-	import { spring } from 'svelte/motion';
-	import { pannable } from './pannable.js';
+	import { spring } from 'svelte/motion'
+	import { pannable } from './pannable.js'
 
-	let { width, height } = window.screen
-	let size = [700, 400]
-	let out = [0, 0]
+	let width, height
+	$: width, height && checkOverflow()
+	export let name, w, h
+
+	$: size = [(width * w) / 1920, (height * h) / 920]
+	$: out = [Math.abs($coords.x) - Math.ceil((width - size[0]) / 2), Math.abs($coords.y) - Math.ceil((height - size[1]) / 2)]
 
 	const coords = spring({ x: 0, y: 0 }, {
 		stiffness: 0.2,
@@ -12,42 +15,37 @@
 	})
 
 	function handlePanStart() {
-		coords.stiffness = coords.damping = 1;
+		coords.stiffness = coords.damping = 1
 	}
 
 	function handlePanMove(event) {
 		coords.update($coords => ({
 			x: $coords.x + event.detail.dx,
 			y: $coords.y + event.detail.dy
-		}));
-		out[0] = Math.abs($coords.x) - (width - size[0]) / 2
-		out[1] = Math.abs($coords.y) - Math.ceil((height - size[1]) / 2)
+		}))
 	}
 
-	function handlePanEnd(event) {
-		coords.stiffness = 0.2;
-		coords.damping  = 0.4;
-
+	function checkOverflow() {
 		if (out[0] > 0) {
 			if ($coords.x > 0) {
 				if (out[1] > 0) {
 					if ($coords.y > 0) {
-						coords.set({ x: $coords.x - out[0], y: $coords.y - out[1] });
+						coords.set({ x: $coords.x - out[0], y: $coords.y - out[1] })
 					} else {
-						coords.set({ x: $coords.x - out[0], y: $coords.y + out[1] });
+						coords.set({ x: $coords.x - out[0], y: $coords.y + out[1] })
 					}
 				} else {
-					coords.set({ x: $coords.x - out[0], y: $coords.y });
+					coords.set({ x: $coords.x - out[0], y: $coords.y })
 				}
 			} else {
 				if (out[1] > 0) {
 					if ($coords.y > 0) {
-						coords.set({ x: $coords.x + out[0], y: $coords.y - out[1] });
+						coords.set({ x: $coords.x + out[0], y: $coords.y - out[1] })
 					} else {
-						coords.set({ x: $coords.x + out[0], y: $coords.y + out[1] });
+						coords.set({ x: $coords.x + out[0], y: $coords.y + out[1] })
 					}
 				} else {
-					coords.set({ x: $coords.x + out[0], y: $coords.y });
+					coords.set({ x: $coords.x + out[0], y: $coords.y })
 				}
 			}
 		}
@@ -55,13 +53,18 @@
 		if (out[1] > 0) {
 			if (out[0] < 0) {
 				if ($coords.y > 0) {
-					coords.set({ x: $coords.x, y: $coords.y - out[1] });
+					coords.set({ x: $coords.x, y: $coords.y - out[1] })
 				} else {
-					coords.set({ x: $coords.x, y: $coords.y + out[1] });
+					coords.set({ x: $coords.x, y: $coords.y + out[1] })
 				}
 			}
 		}
+	}
 
+	function handlePanEnd() {
+		coords.stiffness = 0.2
+		coords.damping  = 0.4
+		checkOverflow()
 	}
 </script>
 
@@ -69,15 +72,8 @@
 
 <div class="box" style="--width: {size[0]}px; --height: {size[1]}px; transform: translate({$coords.x}px,{$coords.y}px)">
 	<div class="head" use:pannable on:panstart={handlePanStart} on:panmove={handlePanMove} on:panend={handlePanEnd}>
-		sussy baki
+		<span class="name">{name}</span>
 	</div>
-</div>
-
-<div class="info">
-	<h2>Info</h2>
-	<p>{width} x {height}</p>
-	<p>{$coords.x} x {$coords.y}</p>
-	<p>{out[0]} - {out[1]}</p>
 </div>
 
 <style>
@@ -89,7 +85,6 @@
 		left: calc(50% - var(--width) / 2);
 		top: calc(50% - var(--height) / 2);
 		border-radius: 6px;
-		
 		background-color: aliceblue;
 	}
 
@@ -97,27 +92,20 @@
 		height: calc(var(--height) / 11);
 		border-radius: 6px 6px 0px 0px;
 		box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.48);
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
 		user-select: none;
 		cursor: grab;
+		display: flex;
+    	align-items: center;
 	}
 
 	.head:active {
 		cursor: grabbing;
 	}
 
-	.info {
+	.name {
 		position: absolute;
-		bottom: 0;
-		right: 0;
-		padding: 50px;
-		text-align: right;
-	}
-
-	:global(body) {
-		overflow: hidden;
-		background-color: #048282;
+		left: calc(var(--width) / 70);
+		font-size: calc(var(--height) / 23);
+		margin-bottom: calc(var(--height) / 200);
 	}
 </style>
