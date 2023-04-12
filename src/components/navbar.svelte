9246e23c3fd819cf093d879e9b86728f8c082b07
Simone Salerno <contact@simonesalerno.it>
2023-04-12 14:24:36 +0200

    Fix margin navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -15,19 +15,17 @@
 </script>
 
 <div class="topnav">
-	<span class="menuBtn">
+	<button class="menuBtn">
 		<box-icon name="grid-alt" />
-	</span>
+	</button>
 	<button class="link-btn">Home</button>
 	<button class="link-btn">About</button>
 	<button class="link-btn">Projects</button>
 	<button class="link-btn">Contact</button>
-	<span class="fullscreenBtn">
-		<button class="no-btn" on:click={fullscreenToggle}>
-			<box-icon id="fullscreenIcon" name="fullscreen" />
-			<box-icon id="exitFullscreenIcon" name="exit-fullscreen" style="display: none;" />
-		</button>
-	</span>
+	<button class="fullscreenBtn" on:click={fullscreenToggle}>
+		<box-icon id="fullscreenIcon" name="fullscreen" />
+		<box-icon id="exitFullscreenIcon" name="exit-fullscreen" style="display: none;" />
+	</button>
 </div>
 
 <style>
@@ -39,40 +37,29 @@
 		display: flex;
 	}
 
-	/* Style the links inside the navigation bar */
-	.topnav span {
-		float: left;
-		text-align: center;
-		padding: 15px 10px 10px 10px;
-		text-decoration: none;
+	.topnav button {
+		background-color: transparent;
+		border: none;
+		cursor: pointer;
+		outline: none;
+		padding: 0;
+	}
+
+	.link-btn {
+		margin-right: 14px;
 		font-size: 18px;
+		text-align: center;
+		font-family: monospace;
 	}
 
 	/* Style the fullscreen button */
 	.fullscreenBtn {
-		margin-left: auto;
-		padding: 15px 16px 10px 16px !important;
+		margin: 11px 16px 10px auto;
 	}
 
 	/* Style the menu button */
 	.menuBtn {
-		padding: 15px 8px 10px 16px !important;
-	}
-
-	.topnav button {
-		background-color: transparent;
-		border: none;
-		margin: 0;
-		padding: 0;
-		cursor: pointer;
-		outline: none;
-	}
-
-	.link-btn {
-		margin: 10px !important;
-		font-size: 18px;
-		text-align: center;
-		font-family: monospace;
+		margin: 14px 16px 10px 16px;
 	}
 
 	box-icon {
